c4956045991d1c5a0d85c58b9fdffe4c3c1b4188
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2023-02-17 12:30:27 +0300

    Update resume.pdf to resume_new.pdf

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
       label: "User click the view resume button",
     });
 
-    openFile("/files/resume.pdf");
+    openFile("/files/resume_new.pdf");
   };
 
   return (
